ba2c8527bb923971b4999788f0f95182f2544131
IgorZyktin <nicord@yandex.ru>
2021-08-17 22:38:17 +0300

    added many spaces folding

--- omoide/application/static/code.js
+++ omoide/application/static/code.js
@@ -1,5 +1,6 @@
 function alterPlus(x) {
     // rewrite + symbol in query into %2B
     let element = document.getElementById("query_element");
-    element.value = element.value.replace('\+', '%2B')
+    element.value = element.value.replaceAll("\+", "%2B")
+    element.value = element.value.replaceAll(/\s+/g, " ")
 }
